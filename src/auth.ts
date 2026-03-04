43d3d763e4a8ea715756470f462ba2a0942a1882
Federico Lo Dolce <flodolce92@gmail.com>
2026-03-04 02:20:33 +0100

    test2

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,18 @@
-import { FORTY_TWO_SCHOOL_CLIENT_ID, FORTY_TWO_SCHOOL_CLIENT_SECRET } from "$env/static/private"
+import {
+  FORTY_TWO_SCHOOL_CLIENT_ID,
+  FORTY_TWO_SCHOOL_CLIENT_SECRET,
+  AUTH_SECRET,
+} from "$env/static/private"
 import { SvelteKitAuth } from "@auth/sveltekit"
 import FortyTwo from "@auth/sveltekit/providers/42-school"
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
-  providers: [FortyTwo({
+  providers: [
+    FortyTwo({
       clientId: FORTY_TWO_SCHOOL_CLIENT_ID,
       clientSecret: FORTY_TWO_SCHOOL_CLIENT_SECRET,
-    }),],
+    }),
+  ],
+  secret: AUTH_SECRET,
+  trustHost: true,
 })
